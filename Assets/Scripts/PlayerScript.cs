e01d848573584e6bd1cc17e12738df848f68778c
unknown <cesilva99@gmail.com>
2020-10-24 22:57:05 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,27 +9,46 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioClip musicClipThree;
+
+    public AudioSource musicSource;
+
     public Text score;
 
+    public Text lives;
+
+    public Text wintext;
+
     private int scoreValue = 0;
+
+    private int livesValue = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        wintext.text = "";
+        musicSource.clip = musicClipThree;
+        musicSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
-        float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -40,6 +59,32 @@ public class PlayerScript : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject); 
+        }
+        if (livesValue == 0)
+        {
+            wintext.text = "You Lose! Game by Cesar Silva";
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+        if (scoreValue == 8)
+        {
+            wintext.text = "You Win! Game by Cesar Silva";
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
 
     }
 
@@ -49,8 +94,14 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 2), ForceMode2D.Impulse);
             }
+            if (scoreValue == 4)
+        {
+            transform.position = new Vector3(27.0f, 50.5f, 0.0f);
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+        }
         }
     }
 }
